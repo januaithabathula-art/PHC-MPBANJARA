45ab4e16f4d1eeb13e1c25a7d9aefa691fdf2ac7
januaithabathula-art <janu.aithabathula@gmail.com>
2026-07-14 09:40:28 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,25 @@
-// PHC Banjara Service Worker - v5 SELF-DESTRUCT
-// This SW unregisters itself to prevent caching issues
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.registration.unregister())
-      .then(() => self.clients.claim())
-  );
+const CACHE = 'phc-drug-board-v1';
+const FILES = ['./index.html', './drugs_data.js', './manifest.json'];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
+  self.skipWaiting();
 });
-// Pass all fetches through without caching
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request));
+
+self.addEventListener('activate', e=>{
+  e.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e=>{
+  if(e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(res=>{
+      const resClone = res.clone();
+      caches.open(CACHE).then(c=>c.put(e.request, resClone));
+      return res;
+    }).catch(()=>caches.match(e.request))
+  );
 });
