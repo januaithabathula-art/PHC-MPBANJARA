c1faa5e7011feb4e2998891e788c2d70707d8e8b
januaithabathula-art <janu.aithabathula@gmail.com>
2026-07-14 21:24:31 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,12 @@
-const CACHE = 'phc-drug-board-v1';
-const FILES = ['./index.html', './drugs_data.js', './manifest.json'];
-
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e=>{
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
-
-self.addEventListener('fetch', e=>{
-  if(e.request.method !== 'GET') return;
-  e.respondWith(
-    fetch(e.request).then(res=>{
-      const resClone = res.clone();
-      caches.open(CACHE).then(c=>c.put(e.request, resClone));
-      return res;
-    }).catch(()=>caches.match(e.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
 });
